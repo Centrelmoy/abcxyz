dcc19c199b6603632c83945d431a071821e693a1
Centrelmoy <hovuanh1102@gmail.com>
2023-09-23 21:34:37 +0700

    buoi6

---
+++ Buoi6/js/main.js
@@ -0,0 +1,24 @@
+var age = 22;
+console.log("Your age", age);
+
+var isAdult = true;
+console.log("You are adult", isAdult);
+
+var firstName = "Anh";
+console.log("Your name is" + firstName);
+
+var phone = null;
+console.log("Your phone number is", phone)
+
+var address = undefined;
+console.log("Your address is", address)
+
+var books = ["Book 1", "All in love", "Book 3", 1];
+console.log("Your favorite books are", books);
+
+var closedFriend = {
+                    "full_name": "Ho Vu Anh",
+                    age: 23,
+                    address: null
+}
+console.log("Your best friend's name", closedFriend.full_name); 
