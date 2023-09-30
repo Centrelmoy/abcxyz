f24fc3aeb34d6954d52ed4a222a59a5d4dad6057
Centrelmoy <hovuanh1102@gmail.com>
2023-09-30 21:39:41 +0700

    buoi6 tt

--- Buoi6/js/main.js
+++ Buoi6/js/main.js
@@ -22,3 +22,87 @@ var closedFriend = {
                     address: null
 }
 console.log("Your best friend's name", closedFriend.full_name); 
+
+// Toan tu so sanh - Comparison Operators
+let a = 5;
+console.log(a + 10);
+
+console.log(a - 10);
+
+console.log(a * 15);
+
+console.log(a ** 2);
+
+console.log(a / 2);
+
+console.log(a % 2);
+
+a++;
+console.log(a);
+
+a--;
+console.log(a);
+
+let x = 12;
+let y = 15;
+let z = "12";
+
+console.log(x == y);
+
+console.log(x === z);
+
+console.log(x !== y);
+
+console.log(x > y);
+
+console.log(x < y);
+
+console.log(x >= y);
+
+console.log(x <= y);
+
+// Toan tu logic - Logical Operators
+let first = x < y;
+let second = x == y;
+
+console.log(first && second);
+
+console.log(first || second);
+
+console.log(!second);
+
+// Toan tu gan - Assignment Operators
+x = y;
+console.log(x);
+
+console.log((x += y));
+
+console.log((x -= y));
+
+console.log((x *= y));
+
+console.log((x /= y));
+
+console.log((x %= y));
+
+// Toan tu 3 ngoi - Conditional Operators
+// (dieukien) ? dieukienDung : dieukienSai
+let varA = (x > y) ? "Co": "Khong";
+console.log(varA);
+
+age = 12;
+if(age > 0) {
+    console.log("You haven't been born")
+}
+if(age < 18) {
+    console.log("You are a teen")
+} else {
+    console.log("You are an adult")
+}
+
+let answer = "A";
+switch (answer) {
+    case "A":
+        console.log("Your ans is A");
+        break;
+}
